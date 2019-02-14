7c2c8e5f0bfca3d49052a59520a45024c34411ed
Alexandre Bouttier <alexandrebouttier@gmail.com>
2019-02-14 12:35:11 +0100

    add compétences

--- functions.php
+++ functions.php
@@ -102,3 +102,4 @@ function pagination( $args = array(), $class = 'pagination' ) {
 
 <?php
 }
+
--- page-templates/about.php
+++ page-templates/about.php
@@ -22,7 +22,29 @@ get_header();
                     <img class="photo" src="<?php the_field('apropos_photo');?>" />
                 </div>
                 <h2>Mes compétences</h2>
+                <?php 
+
+
+                $args = array(  'post_type' => 'competence','meta_key'=> 'valeur_competence','orderby'=> 'meta_value');
+                query_posts($args); ?>
+                <!-- the loop -->
+                <?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
+
+                <div class="text-center"><?php the_title();?> <?php the_field('valeur_competence');?>%</div>
+                <div class="progress">
+                    <div class="progress-bar bg-primary" role="progressbar"
+                        aria-valuenow="<?php the_field('valeur_competence');?>" aria-valuemin="0" aria-valuemax="100"
+                        style="width: <?php the_field('valeur_competence');?>%;"></div>
+                </div>
+                <?php endwhile; wp_reset_query(); endif; ?>
+
+                <div class="bloc-btn-cv"><a rel="noopener noreferrer" target="_blank"
+                        href="<?php the_field('lien_cv');?>"><button class="btn btn-info"><i
+                                class="fas fa-file-alt mr-2"></i>Mon CV</button></a></div>
             </div>
+
+
+
 
             <div class="col-lg-6 wow fadeInUp animated">
                 <div class="presentation">
