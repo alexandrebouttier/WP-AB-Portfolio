6a5b9876290273946644cbf51558f2e659ed8c86
Alexandre Bouttier <alexandrebouttier@gmail.com>
2019-02-25 17:03:06 +0100

    add title

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <title>Alexandre Bouttier Développeur web Wordpress à Rennes</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <?php wp_head(); ?>
